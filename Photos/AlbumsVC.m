1a5dc420585f84a667e47a9a7709f385597e71ce
swt <275712575@qq.com>
2014-03-19 11:40:55 +0800

    添加代码

--- Photos/AlbumsVC.m
+++ Photos/AlbumsVC.m
@@ -27,7 +27,9 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.title = @"Ablums";
+    [self configModel];
+    [self configView];
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -35,7 +37,86 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+#pragma mark - Model
+-(void)configModel
+{
 
+}
+#pragma mark - View
+-(void)configView
+{
+    [self configTitleAndBarButtons];
+    [self configAlert];
+}
+-(void)configTitleAndBarButtons
+{
+    self.title = @"相册";
+    UIBarButtonItem *leftbuttonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(reciveAddButton:)];
+    
+    self.navigationItem.leftBarButtonItem = leftbuttonItem;
+    
+    
+    //    UIButton *editButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    //    [editButton setTitle:@"编辑" forState:UIControlStateNormal];
+    UIBarButtonItem *rightbuttonItem = [[UIBarButtonItem alloc] initWithTitle:@"编辑" style:UIBarButtonItemStylePlain target:self action:@selector(reciveEditButton:)];
+    self.navigationItem.rightBarButtonItem = rightbuttonItem;
+    
+    myTableView = [[UITableView alloc] initWithFrame:self.view.frame];
+    [self.view addSubview:myTableView];
+    
+}
+-(void)configAlert
+{
+    alert = [[UIAlertView alloc] initWithTitle:@"新建相簿" message:@"请为次相簿输入名称" delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"存储", nil];
+    alert.alertViewStyle = UIAlertViewStylePlainTextInput;
+    UITextField *textField = [alert textFieldAtIndex:0];
+    textField.placeholder = @"标题";
+
+}
+
+#pragma mark - Control
+-(void)reciveAddButton:(UIBarButtonItem*)sender
+{
+    
+    UITextField *textField = [alert textFieldAtIndex:0];
+    textField.text = @"";
+    
+    [alert show];
+    
+//    UIButton *saveButton = [alert buttonTitleAtIndex:1];
+//    UITextField *textField = [alert textFieldAtIndex:0];
+    
+}
+
+-(void)reciveEditButton:(UIBarButtonItem*)sender
+{
+    if (myTableView.editing) {
+        [sender setTitle:@"编辑"];
+        [myTableView setEditing:NO animated:YES];
+    }else
+    {
+        [myTableView setEditing:YES animated:YES];
+        [sender setTitle:@"完成"];
+    }
+    
+}
+
+
+#pragma mark - UIAlertViewDelegate
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    
+}
+
+- (BOOL)alertViewShouldEnableFirstOtherButton:(UIAlertView *)alertView
+{
+    UITextField *textField = [alertView textFieldAtIndex:0];
+    if ([textField.text length] == 0)
+    {
+        return NO;
+    }
+    return YES;
+}
 /*
 #pragma mark - Navigation
 
