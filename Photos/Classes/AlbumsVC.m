01b5b959bed772a86ea5f77c6fe3ba3df2066aa7
swt <275712575@qq.com>
2014-03-20 10:11:06 +0800

    ci

--- Photos/Classes/AlbumsVC.m
+++ Photos/Classes/AlbumsVC.m
@@ -14,15 +14,6 @@
 
 @implementation AlbumsVC
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-        
-    }
-    return self;
-}
 - (instancetype)initWithCoder:(NSCoder *)coder
 {
     self = [super initWithCoder:coder];
